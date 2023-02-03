28ab41fcf0d4416b9c8ae336b9b32972f34571d4
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-02-03 23:27:51 +0530

    Model

--- queryraiservibrance/settings.py
+++ queryraiservibrance/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATICFILES_DIRS=[
-        os.path.join(BASE_DIR,'expensemanager/static')
+        os.path.join(BASE_DIR,'queryraiservibrance/static')
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL  = '/media/'
--- services/models.py
+++ services/models.py
@@ -1,4 +1,6 @@
+import shortuuid
 from django.db import models
+
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 from phonenumber_field.modelfields import PhoneNumberField
@@ -8,7 +10,13 @@ class Query(models.Model):
     registration_id = models.CharField(max_length = 200)
     college_name = models.CharField(max_length = 400)
     title = models.CharField(max_length = 30)
-    type = models.CharField(max_length = 40)
     description = HTMLField()
     transaction_id = models.CharField(max_length = 500)
     screenshot = models.ImageField(upload_to = 'references/')
+    slug = models.SlugField(blank = True)
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug = shortuuid.ShortUUID()
+        super(Query, self).save(*args,**kwargs)
+    def __str__(self):
+        return self.title+"-"+self.user_name+"-"+self.slug
